29d1e979983498a29a8d1a815981fabccd374244
Leon Do <leondo@Leons-MacBook-Pro.local>
2016-10-31 12:55:56 -0400

    php files

---
+++ index.php
@@ -0,0 +1,4 @@
+<!-- Just this one line. Notice how the HTML file is passed as the only argument for the function -->
+<?php include_once("index.html"); ?>
+<?php include_once("contact.html"); ?>
+<?php include_once("portfolio.html"); ?>
